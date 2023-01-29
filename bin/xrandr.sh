1da8c4cb3089da76d4f5d30526329a525899f3de
Bernard El-Hagin <bernard.elhagin@gmail.com>
2023-01-29 22:47:36 +0100

    Added virtual option to xrandr.sh

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -3,3 +3,7 @@
 xrandr --newmode "2560x1440" 241.50 2560 2608 2640 2720 1440 1443 1448 1481 +hsync -vsync
 xrandr --addmode VGA-1 "2560x1440"
 xrandr --output VGA-1 --mode 2560x1440
+
+#xrandr --newmode "2560x1440" 241.50 2560 2608 2640 2720 1440 1443 1448 1481 +hsync -vsync
+#xrandr --addmode Virtual1 "2560x1440"
+#xrandr --output Virtual1 --mode 2560x1440
