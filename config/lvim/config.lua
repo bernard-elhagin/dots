0bdc5841e4a4a64804745c1be2f3d3c32826ad0d
Bernard El-Hagin <bernard.elhagin@gmail.com>
2023-11-20 20:46:45 +0100

    [lvim] Changed colorscheme

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -1,6 +1,7 @@
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
-lvim.colorscheme = 'pinkmare'
+--lvim.colorscheme = 'pinkmare'
+lvim.colorscheme = 'tokyonight'
 
 reload("bertold.settings")
 reload("bertold.mappings")
