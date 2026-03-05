dc937d3a64fe85eaa3d0dba1c5728d537a10a031
Bernard El-Hagin <bernard.elhagin@gmail.com>
2026-03-05 12:36:41 +0100

    Modified add_acls.sh

--- bin/add_acls.sh
+++ bin/add_acls.sh
@@ -1,23 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 host=$(hostname -I)
+bootstrap=$(echo "$host":9092 | tr -d ' ')
 
-bootstrap=$(echo "$host":9092 | sed -e 's/ //')
-
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    topic=$(echo "$line" | cut -f 1 -d" ")
-    user=$(echo "$line" | cut -f 2 -d" ")
-    operation=$(echo "$line" | cut -f 3 -d" ")
+while read -r topic user operation; do
 
     if [[ $operation == 'read' ]]; then
         echo "/opt/kafka/bin/kafka-acls.sh --bootstrap-server $bootstrap --add --allow-principal User:$user --operation $operation --topic $topic --command-config /home/elhbe1/kafka/security/client.config"
-        echo "
-        "
+
         /opt/kafka/bin/kafka-acls.sh --bootstrap-server $bootstrap --add --allow-principal User:$user --operation read --group='*' --topic $topic --command-config /home/elhbe1/kafka/security/client.config
     else
         echo "/opt/kafka/bin/kafka-acls.sh --bootstrap-server $bootstrap --add --allow-principal User:$user --operation $operation --topic $topic --command-config /home/elhbe1/kafka/security/client.config"
-        echo "
-        "
+
         /opt/kafka/bin/kafka-acls.sh --bootstrap-server $bootstrap --add --allow-principal User:$user --operation write --topic $topic --command-config /home/elhbe1/kafka/security/client.config
     fi
 
